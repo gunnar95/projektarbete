b6b344d6c55cfc1faef93723ab4d3493ed0c919f
lukastegendal <lukas_tegendal@hotmail.com>
2013-11-08 20:17:03 +0100

    Ändrade så att bildlänken skapas från "USERPROFILE" så att stringen inte ska behöva ändras beroende på vem som kör programmet.

--- projektarbete/src/projektarbete/Main.java
+++ projektarbete/src/projektarbete/Main.java
@@ -1,12 +1,9 @@
 package projektarbete;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {		
 		DisplayImage img = new DisplayImage(System.getenv("USERPROFILE") + "\\Google Drive\\Projektarbete Grafisk bildanalys av havsvågor\\ogm_201309151650 B&WkonverteradJPEG.jpg");
-		System.out.println("hej23");
-	
 	}
 }
