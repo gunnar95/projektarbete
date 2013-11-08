3be9b2e306b4eab20a62a0557b99d6857a16ab89
lukastegendal <lukas_tegendal@hotmail.com>
2013-11-08 20:39:24 +0100

    Fixat problem med git/github

--- projektarbete/src/projektarbete/Main.java
+++ projektarbete/src/projektarbete/Main.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {		
-		DisplayImage img = new DisplayImage(System.getenv("USERPROFILE") + "\\Google Drive\\Projektarbete Grafisk bildanalys av havsvågor\\ogm_201309151650 B&WkonverteradJPEG.jpg");
-		System.out.println("hej23");
-	
+		DisplayImage img = new DisplayImage(System.getenv("USERPROFILE") + "\\Google Drive\\Projektarbete Grafisk bildanalys av havsvågor\\ogm_201309151650 B&WkonverteradJPEG.jpg");	
 	}
 }
