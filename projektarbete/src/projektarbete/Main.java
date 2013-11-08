0d1ad0cf879a7a0578b384353d48ff4c9bcb93a9
Lukas Tegendal <lukas_tegendal@hotmail.com>
2013-11-08 16:52:07 +0100

    Ändrade så att bildsökvägen går från "USERPROFILE".

--- projektarbete/src/projektarbete/Main.java
+++ projektarbete/src/projektarbete/Main.java
@@ -1,11 +1,12 @@
 package projektarbete;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		DisplayImage img = new DisplayImage("C:\\Users\\lukteg1ros\\Google Drive\\Projektarbete Grafisk bildanalys av havsvågor\\ogm_201309151650 B&WkonverteradJPEG.jpg");
+	public static void main(String[] args) throws IOException {		
+		DisplayImage img = new DisplayImage(System.getenv("USERPROFILE") + "\\Google Drive\\Projektarbete Grafisk bildanalys av havsvågor\\ogm_201309151650 B&WkonverteradJPEG.jpg");
 		System.out.println("hej");
+	
 	}
 }
