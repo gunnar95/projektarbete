14abeb9cce9b8a0e2eb919d9b7323c956f2ff834
Lukas Tegendal <lukas_tegendal@hotmail.com>
2013-11-08 16:53:30 +0100

    test

--- projektarbete/src/projektarbete/Main.java
+++ projektarbete/src/projektarbete/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
 	public static void main(String[] args) throws IOException {		
 		DisplayImage img = new DisplayImage(System.getenv("USERPROFILE") + "\\Google Drive\\Projektarbete Grafisk bildanalys av havsvågor\\ogm_201309151650 B&WkonverteradJPEG.jpg");
-		System.out.println("hej");
+		System.out.println("hej23");
 	
 	}
 }
